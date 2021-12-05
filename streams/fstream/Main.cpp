566455d1d7e9e0878d9e472a1164c5df0ff70e30
Pavel Matviyuk <pav.matt@mail.ru>
2021-12-05 14:54:44 +0300

    Fstream task realization
    With string streams words in the line
    are converted into a number. If
    it's successful, program checks if
    number is two-digit or not

--- streams/fstream/Main.cpp
+++ streams/fstream/Main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
-int main()
+bool has_2digit(const std::string & str) {
+	std::stringstream sstream(str);
+	int num;
+	std::string word;
+	while (sstream.good()) {
+		sstream >> word;
+		std::stringstream wordstream(word);
+		if (wordstream >> num && ((num > 9 && num < 100) || (num < -9 && num > -100))) {
+			return true;
+		}
+		else {
+			wordstream.clear();
+		}
+	}
+	return false;
+}
+
+int main(int argc, char **argv)
 {
-    return 0;
+	if (argc != 2) {
+		std::cout << "\n>>> usage: program filename\n";
+		return 1;
+	}
+	std::string filename = argv[1];
+	std::fstream fin(filename);
+	if (!fin.good()) {
+		std::cout << "\n>>> file not found";
+		std::cout << "\n>>> usage: program filename\n";
+		return 1;
+	}
+	else {
+		std::string line;
+		while (std::getline(fin, line)) {
+			if (!has_2digit(line)) {
+				std::cout << line << std::endl;
+			}
+		}
+	}
 }
